5074f3f5d02201b9b97a1e895be16e73eb2ae3a7
BuildTools <cooldaveb@live.co.uk>
2023-03-08 05:38:59 +0000

    Removed unnecessary import

--- src/main/java/team/creative/littletiles/server/level/little/LittleServerPlayerHandler.java
+++ src/main/java/team/creative/littletiles/server/level/little/LittleServerPlayerHandler.java
@@ -17,7 +17,6 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketSendListener;
