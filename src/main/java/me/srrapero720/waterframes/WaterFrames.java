7a77340b28b12fdb819b9edce9bfd33e0c690b63
SrRapero720 <SrRapero720@hotmail.com>
2023-03-10 23:11:48 -0600

    Fixing here, moving that. Keep simple, Keep usefull

--- src/main/java/me/srrapero720/waterframes/WaterFrames.java
+++ src/main/java/me/srrapero720/waterframes/WaterFrames.java
@@ -1,58 +1,43 @@
 package me.srrapero720.waterframes;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import team.creative.littleframes.LittleFrames;
-import team.creative.littleframes.LittleFramesRegistry;
-import team.creative.littleframes.client.LittleFramesClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import team.creative.creativecore.common.config.holder.CreativeConfigRegistry;
+import team.creative.creativecore.common.network.CreativeNetwork;
+import team.creative.littleframes.LittleFramesConfig;
+import me.srrapero720.waterframes.custom.packets.WaterFramePacket;
 
 @Mod(WaterFrames.ID)
 public class WaterFrames {
     public static final String ID = "waterframes";
-
+    public static LittleFramesConfig CONFIG;
+    public static final Logger LOGGER = LogManager.getLogger(ID);
+    public static final CreativeNetwork NETWORK = new CreativeNetwork("1.0", LOGGER, new ResourceLocation(ID, "main"));;
     public WaterFrames() {
         MinecraftForge.EVENT_BUS.register(this);
 
-//        loadLittleTiles();
-        loadLittleFrames();
-    }
-
-    private void loadLittleFrames() {
-//        LittlePacketTypes.init();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(LittleFrames::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(WaterFrames::init);
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> LittleFramesClient.load(FMLJavaModLoadingContext.get().getModEventBus()));
 
         LittleFramesRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         LittleFramesRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         LittleFramesRegistry.BLOCK_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
-    private void loadLittleTiles() {
-//        FMLJavaModLoadingContext.get().getModEventBus().addListener(LittleTiles::init);
-//        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> LittleTilesClient.load(FMLJavaModLoadingContext.get().getModEventBus()));
-//
-//        LittleTilesRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-//        LittleTilesRegistry.BLOCK_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-//        LittleTilesRegistry.ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
 
+
+    public static void init(final FMLCommonSetupEvent event) {
+        CreativeConfigRegistry.ROOT.registerValue(ID, CONFIG = new LittleFramesConfig());
+
+        NETWORK.registerType(WaterFramePacket.class, WaterFramePacket::new);
+    }
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
-
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            //SrConsole.debug(MODULE, "Registrando bloque: " + blockRegistryEvent.getName().toString());
-        }
-
-        @SubscribeEvent
-        public static void onItemsRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
-            //SrConsole.debug(MODULE, "Registrando item: " + itemRegistryEvent.getName().toString());
-        }
     }
 }
